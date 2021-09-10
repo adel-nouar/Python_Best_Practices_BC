dd3d0ac2eb394c6f2ef9205d801237dbee26aae8
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-10 21:09:33 +0200

    Variables names

---
+++ Day_01/demos/d1_naming.py
@@ -0,0 +1,26 @@
+"""
+Demo of naming and some information about PEP8
+www.python.org/dev/peps/pep-0008/
+"""
+
+# Bad examples
+var = ... # doesn't mean something useful 
+data = ... # don't give something useful about what the value is
+temp = ... # It's confusing, it's temperature, temporare ?
+df = ... # Doesn't describe what the data is
+list = ... # List is also a function, we couldn't use it
+print = ... # It a default python function
+l = ... # Don't helpful, doesn't mean anything and could be interpreted as number one
+distance = ... # In kilometers, Miles, or what ?
+fname = ... # Avoid abbreviation when it's possible, use first_name instead
+
+
+# Good examples
+number_of_children = 2
+age = 42
+postal_code = 94500
+
+
+# Two words concatenated : using snake case
+street_name = "Main Street"
+
