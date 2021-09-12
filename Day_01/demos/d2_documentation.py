7a7b3def8a5396d17897b42ccadddc142185219d
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-12 12:44:04 +0200

    docstrings

---
+++ Day_01/demos/d2_documentation.py
@@ -0,0 +1,14 @@
+"""
+Demo documentation (docstrings) --> Describe what generally is done by the module, the function, etc... It's placed at the top
+1) Module level docstring
+2) function docstring
+3) class docstring
+"""
+
+# Comment
+
+def multiply(x, y):
+    """Return the product of x and y."""
+    return x * y
+
+multiply(2,3)
