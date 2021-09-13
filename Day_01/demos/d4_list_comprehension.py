40db9c514e8c3fd01dba916aac0da9ca8418d185
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-13 09:06:37 +0200

    demo list comprehension

---
+++ Day_01/demos/d4_list_comprehension.py
@@ -0,0 +1,40 @@
+"""
+Demo of list comprehensions
+"""
+
+from day1.demos.d1_naming import convert_eur_to_usd
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers_squared = []
+for x in numbers:
+    numbers_squared.append(x ** 2)
+
+print(numbers_squared)
+
+# List comprehension
+numbers_squared = [x ** 2 for x in numbers]
+print(numbers_squared)
+
+
+prices_eur = [100, 80, 95, 200]
+prices_usd = [convert_eur_to_usd(price) for price in prices_eur]
+print(prices_usd)
+
+
+# Extra: dictionary comprehensions
+# Example 1
+squares = {}
+for n in numbers:
+    squares[n] = n ** 2
+print(squares)  # prints {1: 1, 2: 4, 3: 9, 4: 16}
+
+# Dict comprehension
+squares = {x: x ** 2 for x in numbers}
+print(squares)  # prints {1: 1, 2: 4, 3: 9, 4: 16}
+
+
+# Example 2
+employees = {"john": 1234, "claire": 1235, "danny": 1236}
+employees = {v: k for k, v in employees.items()}
+print(employees)
