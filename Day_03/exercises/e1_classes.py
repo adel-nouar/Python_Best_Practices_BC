ba4af86108bdefdce9f5f3a0b939f0d2784c8c96
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-16 19:28:49 +0200

    Day03: Ex1 done

--- Day_03/exercises/e1_classes.py
+++ Day_03/exercises/e1_classes.py
@@ -5,10 +5,31 @@ When you have created your class, you should be able to use it as follows:
 p1 = Person("John", 42)
 print(p1.name)
 print(p1.age)
+"""
+class Person:
+    def __init__(self, name:str, age:int) -> None:
+        self.name = name
+        self.age = age
 
+p1 = Person("John", 42)
+print(p1.name)
+print(p1.age)
+        
+
+"""
 # Exercise 2
 Implement a method called "say_hello()".
 You should be able to use it as follows:
 p1 = Person("John", 42)
 p1.say_hello()  # prints "John says hello"
 """
+class Person:
+    def __init__(self, name:str, age:int) -> None:
+        self.name = name
+        self.age = age
+    
+    def say_hello(self):
+        print(f"{self.name} says hello")
+
+p1 = Person("John", 42)
+p1.say_hello()  # prints "John says hello"
