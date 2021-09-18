447224fa6b88ce93a9b20a3f15870d457f56b004
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-18 16:19:57 +0200

    Day04: ex1 done

--- Day_04/exercises/e1_logging.py
+++ Day_04/exercises/e1_logging.py
@@ -11,23 +11,36 @@ https://docs.python.org/3/library/logging.html
 """
 
 import secrets
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format=format="%(asctime)s %(levelname)-8s %(message)s",
+    filename="e1_logging.log",
+    filemode="a"
+)
 
 def main():
     """Run program to email a secret token"""
+    logging.info("Start program")
     token = generate_token()
     email_token(token)
+    logging.info("End program")
 
 
 def generate_token():
     """Return a token for password reset"""
+    logging.info("Generating token")
     token = secrets.token_hex()
+    logging.info(f"token :{token}")
     return token
 
 
 def email_token(token):
     """Email the token"""
+    logging.info("emailing the token")
     body = f"The token to reset your password: {token}"
+    logging.info(body)
     pass
 
 
