d1a10db111911442ea1d8604030ed15167ce5034
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-18 16:33:40 +0200

    Day04: d2 decorators lesson finished

--- Day_04/demos/d2_decorators.py
+++ Day_04/demos/d2_decorators.py
@@ -1,3 +1,40 @@
 """
-Demo decorators
+Demo decorators : We could extends functionalities to a function
+We could add many decorators for a single function, but best practices to keep at as simple as possible (simple is better than complex)
 """
+
+# Example function 
+def say_hello():
+    print("Hello, how are you?")
+
+
+# 1. Functions as objects
+# greet = say_hello()
+# greet()
+
+# 2. Functions as arguments
+def my_simple_decorator(func):
+    print("Decorating the function")
+    func()
+
+# my_simple_decorator(say_hello)
+
+
+# 3. Docorator
+def my_decorator(func):
+    def wrapper():
+        print("Before calling the decorated function")
+        func() # Here the function is called
+        print("After calling the decorated function")
+    return wrapper()
+
+# say_hello = my_decorator(say_hello)
+
+@my_decorator # -> Those lines are equivalents to say_hello = my_decorator(say_hello)
+def say_hello():
+    print("Hello, how are you?")
+
+
+@my_decorator
+def say_goodbye():
+    print("Bye, bye")
