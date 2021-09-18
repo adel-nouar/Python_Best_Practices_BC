c591e53ad9ecf3226bdfe4f6478a1a0384599a4a
Adel NOUAR <adel.nouar@protonmail.com>
2021-09-18 16:52:08 +0200

    Day04: d3 generators lesson finished

--- Day_04/demos/d3_generators.py
+++ Day_04/demos/d3_generators.py
@@ -1,3 +1,57 @@
 """
 Demo generators
 """
+
+# Iterable
+for x in [1, 2, 3]:
+    print(x)
+
+# Infinite counter (Iterator)
+class MyCounter:
+    def __init__(self, value:int=0) -> None:
+        self.value = value
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        value = self.value
+        self.value += 1
+        return value
+
+counter = MyCounter()
+print(next(counter))
+print(next(counter))
+
+
+def my_generator():
+    a = "Going first"
+    yield a
+    b = "Going second"
+    yield b
+
+gen = my_generator()
+print(next(gen))
+
+for x in gen:
+    print(x)
+
+
+def my_counter(n=0):
+    while True:
+        yield n
+        n += 1
+
+counter = my_counter()
+print(next(counter))
+print(next(counter))
+print(next(counter))
+
+
+# Memory footprint
+import sys
+list_comp = [x for x in range(1000000)]
+generator = (x for x in range(1000000)) # To save space
+
+print(sys.getsizeof(list_comp))
+print(sys.getsizeof(generator))
